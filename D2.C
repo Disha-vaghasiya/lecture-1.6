46453f2c3b8a16ef944446fdef307e676f148970
jenishbabariya22 <dishavaghasiya2311@gmail.com>
2023-11-10 18:28:45 +0530

    first commit

---
+++ D2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a;
+clrscr();
+ printf("Enter the value of A:- ");
+ scanf("%d",&a);
+
+
+ if(a>0){
+	 printf("number is positive...");
+	}else if(a<0){
+		      printf("number is nagative....");
+		     }else if(a==0){
+				   printf("number is netural....");
+				   }
+
+getch();
+}
